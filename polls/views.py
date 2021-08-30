6ce797abb4af3764cc2aed7befa7d55d8d8c9f27
Sujinunt Puangmalai <sujinunt555@hotmail.com>
2021-08-30 17:16:33 +0700

    Add web templates

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+#from django.template import loader
+from django.shortcuts import render
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello World")
+    '''Polls index page that show lastest 5 question'''
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    context = {"latest_question_list": lastest_question_list,}
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    '''Show question detail to User'''
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    '''Show question results to User'''
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request, question_id):
+    '''Show user vote to User'''
+    return HttpResponse(f"You are voting question {question_id}.")
