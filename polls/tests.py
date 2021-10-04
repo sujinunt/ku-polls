1f3ae2e75697aa62a83ed08e25b6d3224ad27747
Sujinunt Puangmalai <32285747+sujinunt@users.noreply.github.com>
2021-10-04 23:34:28 +0700

    Fix test

--- polls/tests.py
+++ polls/tests.py
@@ -147,7 +147,7 @@ class QuestionDetailViewTests(TestCase):
     def test_past_question(self):
         """
         The detail view of a question with a pub_date in the past
-        displays the question's text.
+        displays the question's text. Expect 302.
         """
         past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
