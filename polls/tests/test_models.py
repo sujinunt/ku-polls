c586d37df0ee16969f2f45ddc3ccbde12b9e3a93
Sujinunt Puangmalai <sujinunt555@hotmail.com>
2021-10-28 15:35:16 +0700

    Improve Test

--- polls/tests/test_models.py
+++ polls/tests/test_models.py
@@ -5,20 +5,6 @@ from django.utils import timezone
 from ..models import Question
 
 
-def create_question(question_text, days):
-    """
-
-    Create question function for tested.
-
-    Create a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    time_end = timezone.now() + datetime.timedelta(days=days+3)
-    return Question.objects.create(question_text=question_text, pub_date=time, end_date=time_end)
-
-
 class QuestionModelTests(TestCase):
     """Test Question Model that method is work correctly."""
 
